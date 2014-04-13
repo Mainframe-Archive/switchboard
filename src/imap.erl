c5a882f7d71342d423768fad18fd3a7b2beaca0e
jtmoulia <jtmoulia@gmail.com>
2014-04-12 18:26:12 -0700

    added tokenizing step before parsing

--- src/imap.erl
+++ src/imap.erl
@@ -65,17 +65,32 @@
 -type mailbox() :: binary().
 
 %% Parse Types
--type parse_term() :: binary()               % Atom    [A-Za-z0-9+]+
-                    | integer()              % Number  [0-9]+
-                    | {string, binary()}     % String  "*" | {n}*
-                    | [parse_term()]         % List    (parse_term())
-                    | nil.                   % NIL     NIL
+-type imap_term() :: binary()               % Atom    [A-Za-z0-9+]+
+                   | integer()              % Number  [0-9]+
+                   | {string, binary()}     % String  "*" | {n}*
+                   | [imap_term()]          % List    (imap_term())
+                   | nil.                   % NIL     NIL
 
--type parse_state() :: none
-                     | {string, binary()}
-                     | {atom, binary()}
-                     | {number, binary()}.
+-type token() :: integer()            %% Number
+               | binary()             %% Atom
+               | {string, binary()}   %% String
+               | nil                  %% NIL
+               | crlf                 %% CRLF / \r\n
+               | '(' | ')'
+               | '[' | ']'.
 
+% non quoted terminators:
+%  32 sp
+%  40 (
+%  41 )
+%  91 [
+%  93 ]
+
+-type tokenize_state() :: none
+                        | {quoted, binary()}
+                        | {literal, binary()}
+                        | {number, binary()}
+                        | {atom, binary()}.
 
 -export_type([connspec/0,
               auth_plain/0,
@@ -89,9 +104,8 @@
                 connspec :: connspec(),
                 auth = none :: none | auth(),
                 tag = 0 :: integer(),
-                buffer = <<>> :: binary(),
-                parse_state = none :: parse_state(),
-                token_acc = [] :: [parse_term()],
+                tokenize_state = {<<>>, none} :: {binary(), tokenize_state()},
+                parse_state = {[], []} :: {[token()], [imap_term()]},
                 cmds = gb_trees:empty() :: gb_trees:tree()}).
 
 %%==============================================================================
@@ -106,6 +120,7 @@ start_dispatch() ->
                                    <<"jives48_cars">>}}),
     {ok, Child}.
 -endif.
+
 
 %% @doc start a standalone IMAP connection
 -spec start(connspec()) ->
@@ -136,13 +151,13 @@ cast(Server, Cmd) ->
 
 %% @equiv call(Server, Cmd, ?CALL_TIMEOUT)
 -spec call(pid(), cmd()) ->
-    {ok, [[parse_term()]]} | {error, _}.
+    {ok, _} | {error, _}.
 call(Server, Cmd) ->
     call(Server, Cmd, ?CALL_TIMEOUT).
 
 %% @doc call the command, waiting until timeout for all responses
 -spec call(pid(), cmd(), integer()) ->
-    {ok, [[parse_term()]]} | {error, _}.
+    {ok, _} | {error, _}.
 call(Server, Cmd, Timeout) ->
     gen_server:cast(Server, {cmd, self(), Cmd}),
     Ref = monitor(process, Server),
@@ -153,29 +168,29 @@ call(Server, Cmd, Timeout) ->
 
 %% @equiv recv(?CALL_TIMEOUT)
 -spec recv() ->
-    {ok, [[parse_term()]]} | {error, _}.
+    {ok, _} | {error, _}.
 recv() ->
     recv(?CALL_TIMEOUT).
 
 %% @equiv recv(Timeout, none)
 -spec recv(integer()) ->
-    {ok, [[parse_term()]]} | {error, _}.
+    {ok, _} | {error, _}.
 recv(Timeout) ->
     recv(Timeout, none).
 
 %% @doc receive response until completion message, optional monitor used be call
 -spec recv(integer(), reference() | none) ->
-    {ok, [[parse_term()]]} | {error, _}.
+    {ok, _} | {error, _}.
 recv(Timeout, MonitorRef) ->
     recv(Timeout, MonitorRef, []).
 
 %% @private helper for recieving responses
--spec recv(integer(), reference() | none, [[parse_term()]]) ->
-    {ok, [[parse_term()]]} | {error, _}.
+-spec recv(integer(), reference() | none, _) ->
+    {ok, _} | {error, _}.
 recv(Timeout, MonitorRef, Responses) ->
     receive
         {'*', Response} ->
-            recv(Timeout, [{'*', Response} | Responses]);
+            recv(Timeout, MonitorRef, [{'*', Response} | Responses]);
         {'OK', Response} ->
             {ok, lists:reverse([{'OK', Response} | Responses])};
         {'NO', Response} ->
@@ -230,10 +245,12 @@ handle_cast(_Request, State) ->
 
 
 %% @doc handle messages
-handle_info({ssl, Socket, Data}, #state{socket=Socket, buffer=Buffer} = State) ->
-    ?LOG_DEBUG("data: ~p", [parse(Data)]),
+handle_info({ssl, Socket, Data},
+            #state{socket=Socket, tokenize_state={Buffer, AccState}} = State) ->
+    ?LOG_DEBUG("data: ~p", [Data]),
     %% XXX ineffecient binary cat, buffer can be wrapped in a queue
-    {noreply, churn_buffer(State#state{buffer=(<<Buffer/binary, Data/binary>>)})};
+    Buffer2 = <<Buffer/binary, Data/binary>>,
+    {noreply, churn_buffer(State#state{tokenize_state={Buffer2, AccState}})};
 handle_info({ssl_closed, Socket}, #state{socket=Socket} = State) ->
     ?LOG_DEBUG("Socket Closed: ~p", [self()]),
     {stop, normal, State};
@@ -260,23 +277,25 @@ terminate(Reason, _State) ->
 %% @doc churn the buffer by parsing responses and sending them to the proper processes
 -spec churn_buffer(#state{}) ->
     #state{}.
-churn_buffer(#state{buffer=Buffer, parse_state=ParseState, token_acc=TokenAcc} = State) ->
-    {Result, {Buffer2, ParseState2, TokenAcc2}} = parse(Buffer, ParseState, TokenAcc),
-    churn_buffer(State#state{buffer=Buffer2,
-                             parse_state=ParseState2,
-                             token_acc=TokenAcc2},
+churn_buffer(#state{tokenize_state=TokenizeState, parse_state=ParseState} = State) ->
+    {Result, TokenizeState2, ParseState2} = decode_line(TokenizeState, ParseState),
+    churn_buffer(State#state{tokenize_state=TokenizeState2, parse_state=ParseState2},
                  Result).
 
 
 %% @private internal churn_buffer helper
--spec churn_buffer(#state{}, [parse_term()]) ->
+-spec churn_buffer(#state{}, [imap_term()] | none) ->
     #state{}.
 churn_buffer(State, none) ->
     State;
 churn_buffer(#state{cmds=Cmds} = State, [<<"*">> | Response]) ->
     ?LOG_DEBUG("UNTAGGED: ~p", [Response]),
-    [Pid ! {'*', Response} ||
-        {cmd, Pid, Cmd} <- gb_trees:values(Cmds), cmds_response(Cmd, Response)],
+    ok = lists:foreach(
+           fun({cmd, Pid, _}) ->
+                   Pid ! {'*', Response}
+           end,
+           lists:filter(fun(C) -> cmds_response(C, Response) end,
+                        gb_trees:values(Cmds))),
     churn_buffer(State);
 churn_buffer(State, [<<"+">> | Response]) ->
     ?LOG_DEBUG("Continuation: ~p", [Response]),
@@ -301,10 +320,10 @@ churn_buffer(#state{cmds=Cmds} = State, [Tag | Response]) ->
 -spec cmds_response(_, _) ->
     boolean().
 cmds_response(_Cmd, _Response) ->
-    false.
+    true.
 
 %% @doc return a command as iodata() ready to be sent to the IMAP server
--spec cmd_to_data(internal_cmd()) ->
+-spec cmd_to_data(cmd()) ->
     iodata().
 cmd_to_data(InternalCmd) ->
     %% XXX -- ineffecient end append
@@ -312,7 +331,7 @@ cmd_to_data(InternalCmd) ->
 
 
 %% @doc return the list of command tokens
--spec cmd_to_list(internal_cmd()) ->
+-spec cmd_to_list(cmd()) ->
     iodata().
 cmd_to_list({login, {plain, Username, Password}}) ->
     ["LOGIN", Username, Password];
@@ -354,70 +373,143 @@ start_app(App) ->
 
 
 %%==============================================================================
-%% Response Parsing
+%% Response Tokenizing + Parsing
 %%==============================================================================
 
--type parse_return() :: {[parse_term()] | none,
-                         {binary(), parse_state(), [parse_term()]}}.
 
--spec parse(binary()) ->
-    parse_return().
-parse(Data) ->
-    parse(Data, none, []).
-%% XXX Relies on space after terms -- will drop on \r\n. TOKENIZE + LEX
+-type pop_token_ret() :: {token() | none, binary(), tokenize_state()}.
 
--spec parse(binary(), parse_state(), [parse_term()]) ->
-    parse_return().
-parse(<<>>, ParseState, ParseAcc) ->
-    {none, {<<>>, ParseState, ParseAcc}};
 
-parse(<<$\r, $\n, Rest/binary>>, _, TokenAcc) ->
-    {lists:reverse(TokenAcc), {Rest, none, []}};
+-spec decode_line(binary()) ->
+    {[imap_term()] | none,  binary(), tokenize_state(), [imap_term()]}.
+decode_line(Data) ->
+    decode_line({Data, none}, {[], []}).
 
-parse(<<" ", Rest/binary>>, none, TokenAcc) ->
-    parse(Rest, none, TokenAcc);
+-spec decode_line({binary(), tokenize_state()}, {[token()], [imap_term()]}) ->
+    {[imap_term()] | none,  {binary(), tokenize_state()}, {[token()], [imap_term()]}}.
+decode_line({Data, TokenizeState}, {TokenBuffer, ParseAcc}) ->
+    {Tokens, Data2, TokenizeState2} = tokenize(Data, TokenizeState),
+    {Line, TokenBuffer2, ParseAcc2} = parse(TokenBuffer ++ Tokens, ParseAcc),
+    {Line, {Data2, TokenizeState2}, {TokenBuffer2, ParseAcc2}}.
+
+
+%% @doc tokenize the imap data
+-spec tokenize(binary()) ->
+    {[token()], binary(), tokenize_state()}.
+tokenize(Data) ->
+    tokenize(Data, [], pop_token(Data)).
+
+-spec tokenize(binary(), tokenize_state()) ->
+    {[token()], binary(), tokenize_state()}.
+tokenize(Data, TokenizeState) ->
+    tokenize(Data, [], pop_token(Data, TokenizeState)).
+
+-spec tokenize(binary(), [token()], pop_token_ret()) ->
+    {[token()], binary(), tokenize_state()}.
+tokenize(_Data, Tokens, {none, Rest, TokenizeState}) ->
+    {lists:reverse(Tokens), Rest, TokenizeState};
+tokenize(Data, Tokens, {Token, Rest, TokenizeState}) ->
+    tokenize(Data, [Token | Tokens], pop_token(Rest, TokenizeState)).
+
+
+%% @equiv pop_token(Data, none)
+-spec pop_token(binary()) ->
+    pop_token_ret().
+pop_token(Data) ->
+    pop_token(Data, none).
+
+%% @doc pop the next token from the imap data, returning the state
+-spec pop_token(binary(), tokenize_state()) ->
+    {token() | none, binary(), tokenize_state()}.
+pop_token(<<>>, State) ->
+    {none, <<>>, State};
+
+%% Consume hanging spaces
+pop_token(<<" ", Rest/binary>>, none) ->
+    pop_token(Rest, none);
+
+%% \r\n
+pop_token(<<$\r, $\n, Rest/binary>>, none) ->
+    {crlf, Rest, none};
 
 %% NIL
-parse(<<"NIL", Rest/binary>>, none, TokenAcc) ->
-    parse(Rest, none, [nil | TokenAcc]);
+pop_token(<<"NIL", Rest/binary>>, none) ->
+    {nil, Rest, none};
 
-%% List
-parse(<<$(, Rest/binary>>, none, TokenAcc) ->
-    {List, {InnerRest, none, []}} = parse(Rest, none, []),
-    parse(InnerRest, none, [List | TokenAcc]);
-parse(<<$), Rest/binary>>, _, TokenAcc) ->
-    {lists:reverse(TokenAcc), {Rest, none, []}};
+%% ( | ) | [ | ]
+pop_token(<<$(, Rest/binary>>, none) ->
+    {'(', Rest, none};
+pop_token(<<$), Rest/binary>>, none) ->
+    {')', Rest, none};
 
-%% String
-parse(<<$", Rest/binary>>, none, TokenAcc) ->
-    parse(Rest, {string, <<>>}, TokenAcc);
-parse(<<$", Rest/binary>>, {string, StringAcc}, TokenAcc) ->
-    parse(Rest, none, [{string, StringAcc} | TokenAcc]);
-parse(<<$\\, $", Rest/binary>>, {string, StringAcc}, TokenAcc) ->
-    parse(Rest, {string, <<StringAcc/binary, $">>}, TokenAcc);
-parse(<<C, Rest/binary>>, {string, StringAcc}, TokenAcc) when C /= $"->
-    parse(Rest, {string, <<StringAcc/binary, C>>}, TokenAcc);
+pop_token(<<$[, Rest/binary>>, none) ->
+    {'[', Rest, none};
+pop_token(<<$], Rest/binary>>, none) ->
+    {']', Rest, none};
 
-%% Number
-parse(<<" ", Rest/binary>>, {number, NumberAcc}, TokenAcc) ->
-    parse(Rest, none, [binary_to_integer(NumberAcc) | TokenAcc]);
-    %parse(Rest, none, [NumberAcc | TokenAcc]);
-parse(<<D, Rest/binary>>, {number, NumberAcc}, TokenAcc) when D >= 48, D < 57 ->
-    parse(Rest, {number, <<NumberAcc/binary, D>>}, TokenAcc);
-parse(<<D, Rest/binary>>, none, TokenAcc) when D >= 48, D < 57 ->
-    parse(Rest, {number, <<D>>}, TokenAcc);
-parse(<<C, Rest/binary>>, {number, NumberAcc}, TokenAcc) when C >= 35, C < 123 ->
-    parse(Rest, {atom, <<NumberAcc/binary, C>>}, TokenAcc);
+%% Numbers
+pop_token(<<C, _/binary>> = Data, {number, NumberAcc}) when
+  C =:= 32; C =:= 40; C =:= 41; C =:= 91; C =:= 93 ->
+    {binary_to_integer(NumberAcc), Data, none};
+pop_token(<<$\r, $\n, _/binary>> = Data, {number, NumberAcc}) ->
+    {binary_to_integer(NumberAcc), Data, none};
+pop_token(<<" ", Rest/binary>>, {number, NumberAcc}) ->
+    {binary_to_integer(NumberAcc), Rest, none};
+pop_token(<<D, Rest/binary>>, {number, NumberAcc}) when D >= 48, D < 57 ->
+    pop_token(Rest, {number, <<NumberAcc/binary, D>>});
+pop_token(<<D, Rest/binary>>, none) when D >= 48, D < 57 ->
+    pop_token(Rest, {number, <<D>>});
+pop_token(<<C, Rest/binary>>, {number, NumberAcc}) when C >= 35, C < 123 ->
+    pop_token(Rest, {atom, <<NumberAcc/binary, C>>});
 
 %% Atom
-%% XXX -- atoms as atoms +: simpler syntax, quicker matching, -: could blow the atom table
-parse(<<" ", Rest/binary>>, {atom, AtomAcc}, TokenAcc) ->
-    %% otherwise, {atom, <<"atomname">>}
-    parse(Rest, none, [AtomAcc | TokenAcc]);
-parse(<<C, Rest/binary>>, none, TokenAcc) when C >= 35, C < 123 ->
-    parse(Rest, {atom, <<C>>}, TokenAcc);
-parse(<<C, Rest/binary>>, {atom, AtomAcc}, TokenAcc) when C >= 35, C < 123 ->
-    parse(Rest, {atom, <<AtomAcc/binary, C>>}, TokenAcc).
+pop_token(<<C, _/binary>> = Data, {atom, AtomAcc}) when
+  C =:= 32; C =:= 40; C =:= 41; C =:= 91; C =:= 93 ->
+    {AtomAcc, Data, none};
+pop_token(<<$\r, $\n, _/binary>> = Data, {atom, AtomAcc}) ->
+    {AtomAcc, Data, none};
+pop_token(<<C, Rest/binary>>, none) when C >= 35, C < 123 ->
+    pop_token(Rest, {atom, <<C>>});
+pop_token(<<C, Rest/binary>>, {atom, AtomAcc}) when C >= 35, C < 123 ->
+    pop_token(Rest, {atom, <<AtomAcc/binary, C>>});
+
+%% Quoted Strings
+pop_token(<<$", Rest/binary>>, none) ->
+    pop_token(Rest, {quoted, <<>>});
+pop_token(<<$\\, C, Rest/binary>>, {quoted, Acc}) ->
+    pop_token(Rest, {quoted, <<Acc/binary, C>>});
+pop_token(<<$", Rest/binary>>, {quoted, Acc}) ->
+    {{string, Acc}, Rest, none};
+pop_token(<<$\r, $\n, _>>, {quoted, _}) ->
+    throw({error, crlf_in_quoted});
+pop_token(<<C, Rest/binary>>, {quoted, Acc}) ->
+    pop_token(Rest, {quoted, <<Acc/binary, C>>}).
+
+
+% -type parse_return() :: {[parse_term()] | none,
+%                         {binary(), parse_state(), [parse_term()]}}.
+
+-spec parse([token()]) ->
+    {[imap_term()] | none, [token()], [imap_term()]}.
+parse(Tokens) ->
+    parse(Tokens, []).
+
+-spec parse([token()], [imap_term()]) ->
+    {[imap_term()] | none, [token()], [imap_term()]}.
+parse([], ParseAcc) ->
+    {none, [], ParseAcc};
+
+parse([crlf | Rest], ParseAcc) ->
+    {lists:reverse(ParseAcc), Rest, []};
+
+parse(['(' | Rest], ParseAcc) ->
+    {List, Rest2, []} = parse(Rest),
+    parse(Rest2, [List | ParseAcc]);
+parse([')' | Rest], ParseAcc) ->
+    {lists:reverse(ParseAcc), Rest, []};
+
+parse([Token | Rest], ParseAcc) ->
+    parse(Rest, [Token | ParseAcc]).
 
 
 %%==============================================================================
@@ -429,6 +521,34 @@ parse(<<C, Rest/binary>>, {atom, AtomAcc}, TokenAcc) when C >= 35, C < 123 ->
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
+%% Tokenize
+pop_token_test_() ->
+    [pop_token_atoms(),
+     pop_token_numbers(),
+     pop_token_quoted()].
+
+pop_token_atoms() ->
+    [?_assertEqual({<<"atom">>, <<" ">>, none}, pop_token(<<"atom ">>)),
+     ?_assertEqual({<<"atom123">>, <<" ">>, none}, pop_token(<<"atom123 ">>))].
+
+pop_token_numbers() ->
+    [?_assertEqual({0, <<" ">>, none}, pop_token(<<"0 ">>)),
+     ?_assertEqual({123, <<" ">>, none}, pop_token(<<"123 ">>))].
+
+pop_token_quoted() ->
+    [?_assertEqual({{string, <<"quoted">>}, <<>>, none}, pop_token(<<"\"quoted\"">>))].
+
+
+tokenize_test_() ->
+    [tokenize_terms()].
+
+tokenize_terms() ->
+    [?_assertEqual({[1, 2, <<"a">>], <<>>, none}, tokenize(<<"1 2 a ">>)),
+     ?_assertEqual({[1, 2, '(', <<"a">>, ')', {string, <<"s">>}], <<>>, none},
+                   tokenize(<<"1 2 (a)\"s\"">>))].
+
+
+%% Parse
 parse_test_() ->
     [parse_atoms(),
      parse_numbers(),
@@ -436,31 +556,34 @@ parse_test_() ->
      parse_strings(),
      parse_lists()].
 
-
 parse_atoms() ->
-    [?_assertEqual({none, {<<>>, none, [<<"atom">>]}},
-                   parse(<<"atom ">>))].
+    [?_assertEqual({none, [], [<<"atom">>]},
+                   parse([<<"atom">>]))].
 
 parse_nils() ->
-    [?_assertEqual({none, {<<>>, none, [nil]}}, parse(<<"NIL">>)),
-     ?_assertEqual({none, {<<>>, none, [nil]}}, parse(<<"NIL ">>))].
+    [?_assertEqual({none, [], [nil]}, parse([nil]))].
 
 
 parse_numbers() ->
-    [?_assertEqual({[0], {<<>>, none, []}}, parse(<<"0 \r\n">>)),
-     ?_assertEqual({[21], {<<>>, none, []}}, parse(<<"21 \r\n">>))].
+    [?_assertEqual({[0], [], []}, parse([0, crlf])),
+     ?_assertEqual({[21], [], []}, parse([21, crlf]))].
 
 parse_strings() ->
-    [?_assertEqual({none, {<<>>, none, [{string, <<"string">>}]}},
-                   parse(<<"\"string\"">>))].
+    [?_assertEqual({[{string, <<"string">>}], [], []},
+                   parse([{string, <<"string">>}, crlf]))].
 
 parse_lists() ->
-    [?_assertEqual({none, {<<>>, none, [[{string, <<"a">>}, {string, <<"b">>}]]}},
-                   parse(<<"(\"a\" \"b\")">>)),
-     ?_assertEqual({[[{string, <<"a">>}, 1,
-                      [{string, <<"c">>}, {string, <<"d">>}]],
-                     {string, <<"e">>}, {string, <<"f">>}],
-                    {<<>>, none, []}},
-                   parse(<<"(\"a\" 1 (\"c\" \"d\")) \"e\" \"f\"\r\n">>))].
+    [?_assertEqual({[1, [2, <<"c">>], 4], [], []},
+                   parse([1, '(', 2, <<"c">>, ')', 4, crlf]))].
+
+
+%% decode_line testing
+decode_line_test_() ->
+    [decode_line_default()].
+
+
+decode_line_default() ->
+    [?_assertEqual({[1, 2, 3], {<<>>, none}, {[], []}},
+                   decode_line(<<"1 2 3\r\n">>))].
 
 -endif.
